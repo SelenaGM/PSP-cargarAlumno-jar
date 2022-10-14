ae5ed3b719d2f64d389fc75895639fbe8d56a94b
Selena <selena.garciamerino@cieep.com>
2022-10-14 17:22:04 +0200

    Initial commit

---
+++ src/com/company/cargarAlumno.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class cargarAlumno {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String destino = sc.nextLine();
+        ArrayList<String> lineas = new ArrayList<>();
+        File fichero = new File(destino);
+
+        FileReader fr = null;
+        try {
+            fr = new FileReader(fichero);
+            BufferedReader bf = new BufferedReader(fr);
+
+            String linea = null;
+            while ((linea=bf.readLine())!= null){
+                lineas.add(linea);
+            }
+            if(lineas.size()>0) {
+                for (int i = 0; i < lineas.size(); i++) {
+                    String l = lineas.get(i);
+                    String[] datos =l.split(";");
+                    String nombre = datos[0];
+                    String apellido = datos[1];
+                    String dni = datos[2];
+                    System.out.println(nombre);
+                    System.out.println(apellido);
+                    System.out.println(dni);
+                    System.out.println();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
